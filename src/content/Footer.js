ef2b36a73a855615185a9e9a4844660e1a21ebe3
biggymarley <afaragi@student.1337.ma>
2021-04-03 18:08:32 +0100

    login page design

--- src/content/Footer.js
+++ src/content/Footer.js
@@ -1,13 +1,45 @@
-import './style/style.css'
+import "./style/style.css";
+import axios from "axios";
+// import Grid from "@material-ui/core/Grid";
+import { Paper, GridList , GridListTile} from "@material-ui/core";
+// import { Component } from "react";
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://montanaflynn-lorem-text-generator.p.rapidapi.com/paragraph',
+//   params: {length: '3', count: '1'},
+//   headers: {
+//     'x-rapidapi-key': 'SIGN-UP-FOR-KEY',
+//     'x-rapidapi-host': 'montanaflynn-lorem-text-generator.p.rapidapi.com'
+//   }
+// };
+
+
+
+
 
 function Footer() {
-    return (
-      <div className="Footer">
-      <h1>
-          Welcome IN MATCHA
-      </h1>
-      </div>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <div className="Footer">
+      {/* <GridList
+        // container
+        // direction="row"
+        // className="countainer"
+        // spacing={8}
+        // justify="space-between"
+        cols={2.5}
+      >
+        <GridListTile item xs={3} className="rev-footer">
+        <Paper>
+          d do duis sunt tempor et tempor laborum labore deserunt qui deserunt
+          elit minim. Velit Lorem laborum aute tempor occaecat aliquip nostrud.
+          Nisi quis ut consectetur do eu anim. Ipsum quis in aute dolor quis ad
+          </Paper>
+          </GridListTile>
+        </GridList> */}
+    </div>
+  );
+}
+
+export default Footer;
