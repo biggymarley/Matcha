31b1ba5720cd4c07919a43827ae124cf6626dfd6
biggymarley <afaragi@student.1337.ma>
2021-04-03 19:28:24 +0100

    trying some apis

--- src/content/Footer.js
+++ src/content/Footer.js
@@ -2,24 +2,32 @@ import "./style/style.css";
 import axios from "axios";
 // import Grid from "@material-ui/core/Grid";
 import { Paper, GridList , GridListTile} from "@material-ui/core";
-// import { Component } from "react";
-
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://montanaflynn-lorem-text-generator.p.rapidapi.com/paragraph',
-//   params: {length: '3', count: '1'},
-//   headers: {
-//     'x-rapidapi-key': 'SIGN-UP-FOR-KEY',
-//     'x-rapidapi-host': 'montanaflynn-lorem-text-generator.p.rapidapi.com'
-//   }
-// };
-
-
+import { useState, useEffect } from "react";
 
 
 
 function Footer() {
+  
+  const [data , setData] = useState(null);
+  
+  const options = {
+    method: 'GET',
+    url: 'https://covid-19-data.p.rapidapi.com/report/country/name',
+    params: {date: '2020-04-01', name: 'Italy'},
+    headers: {
+      'x-rapidapi-key': 'a6724ce4f3msh8dc6622e8ded0c1p1384e4jsn6689504d9a4b',
+      'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
+    }
+  };
+  
+  useEffect(() => {
+    axios.request(options).then(res => {
+      console.log(res.data);
+      setData(res.data)
+    })
+  }, []);
+
+
   return (
     <div className="Footer">
       {/* <GridList
