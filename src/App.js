d1e081bf403aa71c195d0ab622d11d89a09fe9b1
biggymarley <afaragi@student.1337.ma>
2021-04-05 18:28:18 +0100

    login page in

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import './App.css';
 import Footer from './content/Footer.js'
 import Header from './content/Header.js'
 import './content/style/style.css'
+import { Button } from '@material-ui/core';
 function App() {
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > (window.innerHeight / 2))
+    document.querySelector('.swip').style.opacity = 0;
+    else if(window.pageYOffset > 100)
+      document.querySelector('.swip').style.opacity = 0.5;
+    else
+    document.querySelector('.swip').style.opacity = 1;
+  });
+
+
   return (
     <div className="body">
     <Header />
     <div className="home">
+      <div className="swip">
+      <span className="swip-left">
+        Swip Left<sup>®</sup>
+        </span>
+        <Button variant="contained" className="create-but">
+            Create ACCOUNT
+        </Button>
+      </div>
     </div>
-    <Footer />
+    <Footer/>
     </div>
   );
 }
--- src/content/Footer.js
+++ src/content/Footer.js
@@ -2,50 +2,42 @@ import "./style/style.css";
 import axios from "axios";
 // import Grid from "@material-ui/core/Grid";
 import { Paper, GridList , GridListTile} from "@material-ui/core";
-import { useState, useEffect } from "react";
+import { useState, useEffect, React } from "react";
 
 
 
 function Footer() {
   
-  const [data , setData] = useState(null);
+  // const [data , setData] = useState([]);
   
-  const options = {
-    method: 'GET',
-    url: 'https://covid-19-data.p.rapidapi.com/report/country/name',
-    params: {date: '2020-04-01', name: 'Italy'},
-    headers: {
-      'x-rapidapi-key': 'a6724ce4f3msh8dc6622e8ded0c1p1384e4jsn6689504d9a4b',
-      'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
-    }
-  };
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://loripsum.net/api/10/short'
+  // };
   
-  useEffect(() => {
-    axios.request(options).then(res => {
-      console.log(res.data);
-      setData(res.data)
-    })
-  }, []);
+  // useEffect(() => {
+  //   axios.request(options).then(res => {
+  //     var parser = new DOMParser();
+	//     var doc = parser.parseFromString(res.data, 'text/html');
+  //     const fata = [...doc.body.children];
+  //     setData(fata);
+  //   }).catch(e => {
+  //     console.log(e);
+  //   })
+  // }, []);
 
-
+  
   return (
     <div className="Footer">
-      {/* <GridList
-        // container
-        // direction="row"
-        // className="countainer"
-        // spacing={8}
-        // justify="space-between"
-        cols={2.5}
-      >
-        <GridListTile item xs={3} className="rev-footer">
-        <Paper>
-          d do duis sunt tempor et tempor laborum labore deserunt qui deserunt
-          elit minim. Velit Lorem laborum aute tempor occaecat aliquip nostrud.
-          Nisi quis ut consectetur do eu anim. Ipsum quis in aute dolor quis ad
-          </Paper>
+      <GridList className="gridlist" cols={2}> 
+      {/* {
+        data.map((child) => (
+          <GridListTile className="tile" >
+            {<p className="rev" >{child.innerHTML}</p>}
           </GridListTile>
-        </GridList> */}
+
+        ))} */}
+        </GridList>
     </div>
   );
 }
